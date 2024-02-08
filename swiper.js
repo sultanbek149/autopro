ba9855857c391f7cc6eea482adf8fa26841c344d
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2024-02-08 20:13:29 +0600

    23

--- swiper.js
+++ swiper.js
@@ -3,6 +3,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     speed: 1500,
+    cssMode: true,
     autoplay: {
         delay: 2000,
         disableOnInteraction: false,
